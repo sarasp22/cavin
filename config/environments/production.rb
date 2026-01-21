8123ed19929d99beaf8fb7d5b104246a3bf92b55
Spadari Sara <saraspadari@yahoo.com>
2026-01-21 14:01:50 +0100

    Disable asset compression to fix Tailwind v4 SassC conflict

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,9 @@ Rails.application.configure do
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   # config.public_file_server.enabled = false
 
-  # Compress CSS using a preprocessor.
+ # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
+  config.assets.css_compressor = nil
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
